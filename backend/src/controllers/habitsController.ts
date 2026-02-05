ca904811aa8e7a9534e68585dc39428b68a5de6a
NiketNannavare <niketnannaware703@gmail.com>
2026-02-05 21:55:38 +0530

    feat: new habits update route

--- backend/src/controllers/habitsController.ts
+++ backend/src/controllers/habitsController.ts
@@ -34,3 +34,18 @@ export const deleteHabit = async (req: Request, res: Response) => {
     res.status(500).json({ error: error });
   }
 };
+
+export const updateHabit = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { name, status, date } = req.body;
+    const updateHabit = await Habits.findByIdAndUpdate(
+      id,
+      { name, status, date },
+      { new: true },
+    );
+    res.status(200).json(updateHabit);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
--- backend/src/routes/habitsRoutes.ts
+++ backend/src/routes/habitsRoutes.ts
@@ -1,9 +1,10 @@
 import express, { RequestHandler } from "express";
-import { createHabit, deleteHabit, getAllHabits } from "../controllers/habitsController";
+import { createHabit, deleteHabit, getAllHabits, updateHabit } from "../controllers/habitsController";
 
 
 const router = express.Router();
 router.post("/api/habits", createHabit as unknown as RequestHandler);
 router.get("/api/habits", getAllHabits as unknown as RequestHandler);
 router.delete("/api/habits/:id", deleteHabit as unknown as RequestHandler);
+router.put("/api/habits/:id", updateHabit as unknown as RequestHandler);
 export default router;
