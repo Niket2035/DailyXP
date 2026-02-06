72dbd3ee9b6b765772e2d14abea711ba45ebf871
NiketNannavare <niketnannaware703@gmail.com>
2026-02-06 23:32:14 +0530

    feat: habits tracking backend logic

---
+++ backend/src/controllers/habitsTrackingController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import habitsTrackingModel from "../models/habitsTrackingModel";
+
+export const updateHabitStatus = async (req: Request, res: Response) => {
+  try {
+    const { habitId, date, status } = req.body;
+    const updated = await habitsTrackingModel.findOneAndUpdate(
+      { habitId, date },
+      { status },
+      { upsert: true, new: true },
+    );
+    res.status(200).json(updated);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
--- backend/src/models/habitsModel.ts
+++ backend/src/models/habitsModel.ts
@@ -2,8 +2,6 @@ import { Schema,model } from "mongoose";
 
 const HabitsSchema = new Schema({
     name: { type: String, required: true },
-    status:{ type: String, enum: ['completed', 'missed', 'partial'], default: 'missed' },
-    date: { type: Date, required: true },
 });
 
 export default model("Habits", HabitsSchema);
