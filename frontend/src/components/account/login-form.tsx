44b002c9c2ed0f4223c167f7bf90c882d5e5a455
NiketNannavare <niketnannaware703@gmail.com>
2026-02-11 23:41:42 +0530

    feat: login page and register page

---
+++ frontend/src/components/account/login-form.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useState } from "react";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+      <div className="w-full max-w-md bg-white  shadow-xl rounded-2xl p-8 border ">
+
+        <h2 className="text-2xl font-bold text-center">
+          Login to your account
+        </h2>
+
+        <p className="text-sm text-center mt-2">
+          Enter your email below to login
+        </p>
+
+        <form onSubmit={handleLogin} className="mt-6 space-y-4">
+
+          <div>
+            <label className="block text-sm font-medium ">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="name@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="mt-1 w-full px-4 py-2 border rounded-lg bg-gray-50  focus:ring-2 focus:ring-black dark:focus:ring-white outline-none"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full py-2.5 rounded-lg bg-black text-white font-medium hover:opacity-90 transition"
+          >
+            Login
+          </button>
+        </form>
+
+        <p className="text-sm text-center mt-6 text-gray-600 dark:text-gray-400">
+          Don't have an account?{" "}
+          <a href="/register" className="font-medium hover:underline">
+            Register
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/account/register-form.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { useState } from "react";
+
+export default function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100  px-4">
+      <div className="w-full max-w-md bg-white  shadow-xl rounded-2xl p-8 border ">
+
+        <h2 className="text-2xl font-bold text-center">
+          Create an account
+        </h2>
+
+        <p className="text-sm text-center mt-2">
+          Enter your details below to create your account
+        </p>
+
+        <form onSubmit={handleRegister} className="mt-6 space-y-4">
+
+          <div>
+            <label className="block text-sm font-medium">
+              Name
+            </label>
+            <input
+              type="text"
+              placeholder="John Doe"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              className="mt-1 w-full px-4 py-2 border rounded-lg bg-gray-50 focus:ring-2 focus:ring-black dark:focus:ring-white outline-none"
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium ">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="name@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="mt-1 w-full px-4 py-2 border rounded-lg bg-gray-50  focus:ring-2 focus:ring-black dark:focus:ring-white outline-none"
+            />
+          </div>
+
+           <button
+            type="submit"
+            className="w-full py-2.5 rounded-lg bg-black text-white font-medium hover:opacity-90 transition"
+          >
+            Register
+          </button>
+        </form>
+
+        {/* <div className="flex items-center my-6">
+          <div className="flex-1 border-t dark:border-gray-700"></div>
+          <span className="px-3 text-sm text-gray-500">
+            OR CONTINUE WITH
+          </span>
+          <div className="flex-1 border-t dark:border-gray-700"></div>
+        </div>
+
+        <button className="w-full py-2.5 border rounded-lg dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 transition">
+          Continue with Google
+        </button> */}
+
+        <p className="text-sm text-center mt-6 text-gray-600 dark:text-gray-400">
+          Already have an account?{" "}
+          <a href="/login" className="font-medium hover:underline">
+            Login
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
