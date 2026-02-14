9e8d7a793c8d0ca960d7a2e0a62593b578d44121
NiketNannavare <niketnannaware703@gmail.com>
2026-02-15 02:32:12 +0530

    feat: JWt auth setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import sleepTrackerRoutes from "./routes/sleepTrackerRoutes";
 import habitsRoutes from "./routes/habitsRoutes";
+import authRoutes from "./routes/authRoutes";
 
 const app = express();
 
@@ -10,7 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(sleepTrackerRoutes);
-app.use(habitsRoutes)
+app.use(habitsRoutes);
+app.use(authRoutes);
 
 
 export default app;
